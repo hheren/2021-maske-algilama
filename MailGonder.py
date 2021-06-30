40b4ea751cdf05451453029cd239ccbed2d4f2ec
Hasan <78102927+hheren@users.noreply.github.com>
2021-06-30 14:05:30 +0300

    Delete MailGonder.py

--- MailGonder.py
+++
@@ -1,9 +0,0 @@
-import smtplib
-
-def MailGonder(mesaj):
-    content = mesaj
-    mail = smtplib.SMTP("smtp.gmail.com",587)
-    mail.ehlo()
-    mail.starttls()
-    mail.login("hhheeerrr15678@gmail.com","450509600")
-    mail.sendmail("hhheeerrr15678@gmail.com","hhheeerrr15678@gmail.com",content)
